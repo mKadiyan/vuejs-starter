4ea860683d6a54ffe8bc076bfe2015821ccc8461
Ravinder Kadiyan <rkadiyan@thoughtworks.com>
2018-08-27 21:33:39 +0530

    add v-once example

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 new Vue({
   el: "#app",
   data: {
-    title: "This is my title"
+    title: "This is my title",
+    link: 'https://www.google.com'
   },
   methods: {
     changeTitle: function(event) {
       this.title = event.target.value;
     },
     sayHello: function() {
-      return "Hey , Hello!";
+        this.title = 'Hello World!';
+        return this.title;
     }
   }
 });
