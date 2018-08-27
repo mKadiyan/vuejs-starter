34dd1eed6b33eaf1323a7c3f125c6b7f9c44eba0
Ravinder Kadiyan <rkadiyan@thoughtworks.com>
2018-08-27 21:54:12 +0530

    add few more elements usage

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ new Vue({
   el: "#app",
   data: {
     title: "This is my title",
-    link: 'https://www.google.com',
+    link: "https://www.google.com",
     finishedLink: '<a href="https://www.google.com">Finished Link</a>'
   },
   methods: {
@@ -10,8 +10,22 @@ new Vue({
       this.title = event.target.value;
     },
     sayHello: function() {
-        this.title = 'Hello World!';
-        return this.title;
+      this.title = "Hello World!";
+      return this.title;
     }
+  }
+});
+
+new Vue({
+  el: "#exercise",
+  data: {
+      name: 'Ravinder Kadiyan',
+      age: 22,
+      myImage: 'https://cdn.tinybuddha.com/wp-content/uploads/2015/10/Having-Fun.png'
+  },
+  methods: {
+      getRandomNumber: function(){
+          return Math.random();
+      }
   }
 });
