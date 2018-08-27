9eda05377e941a1a231ac86887ad0d03ba72ddb4
Ravinder Kadiyan <rkadiyan@thoughtworks.com>
2018-08-27 20:28:16 +0530

    add input event

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ new Vue({
     el: '#app',
     data:{
         title: 'This is my title'
+    },
+    methods:{
+        changeTitle: function(event){
+            this.title =  event.target.value;
+        }
     }
 });
