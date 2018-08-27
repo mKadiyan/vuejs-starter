2c2197a0b705019c4aa1353c20e52cd7235a9196
Ravinder Kadiyan <rkadiyan@thoughtworks.com>
2018-08-27 21:40:42 +0530

    add raw html handling

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ new Vue({
   el: "#app",
   data: {
     title: "This is my title",
-    link: 'https://www.google.com'
+    link: 'https://www.google.com',
+    finishedLink: '<a href="https://www.google.com">Finished Link</a>'
   },
   methods: {
     changeTitle: function(event) {
